37360d3fdbcdc8d8225ddea767ea3b0f567f67da
baneet <bkj6@njit.edu>
2018-02-26 18:38:57 -0500

    Procfile for Heroku

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -22,7 +22,7 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+                    <img class="second-slide" src="http://globalmedicalco.com/photos/globalmedicalco/2/8461.jpg" alt="Second slide">
                     <div class="container">
                         <div class="carousel-caption">
                             <h1>Another example headline.</h1>
@@ -32,7 +32,7 @@
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+                    <img class="third-slide" src="https://lh3.googleusercontent.com/imrQ8FaGZ63Ew4fXWNsEOVfowa5axvc5eHjtNJumbQdOQ9jOhsZBBhmSKheG0ZOt9LM=h900" alt="Third slide">
                     <div class="container">
                         <div class="carousel-caption text-right">
                             <h1>One more for good measure.</h1>
@@ -62,19 +62,19 @@
             <!-- Three columns of text below the carousel -->
             <div class="row">
                 <div class="col-lg-4">
-                    <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+                    <img class="rounded-circle" src="http://celebwallpapers.net/wp-content/uploads/2017/11/hd-background-images-beautiful-background-hd-wallpapers-pulse-of-hd-background-images.jpg" alt="Generic placeholder image" width="140" height="140">
                     <h2>Heading</h2>
                     <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
-                    <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+                    <img class="rounded-circle" src="http://www.amazingwallpaperz.com/wp-content/uploads/Nature-Rock-Background-HD-Wallpapers.jpg" alt="Generic placeholder image" width="140" height="140">
                     <h2>Heading</h2>
                     <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div><!-- /.col-lg-4 -->
                 <div class="col-lg-4">
-                    <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+                    <img class="rounded-circle" src="https://lh3.googleusercontent.com/-DrUHoty_hP4/VuDTsKkIy5I/AAAAAAAAA24/MwtuNsVXdDM/s640/Ankit%252520edit94386.jpg" alt="Generic placeholder image" width="140" height="140">
                     <h2>Heading</h2>
                     <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
@@ -104,7 +104,7 @@
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+                    <img class="featurette-image img-fluid mx-auto" data-src="http://www.powerpointhintergrund.com/uploads/2017/06/--beautiful-wallpaper-backgrounds-and-background-hd-wallpaper-31.jpeg" alt="Generic placeholder image">
                 </div>
             </div>
 
@@ -116,7 +116,7 @@
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div class="col-md-5">
-                    <img class="https://static.pexels.com/photos/531880/pexels-photo-531880.jpeg" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+                    <img class="https://static.pexels.com/photos/531880/pexels-photo-531880.jpeg" data-src="http://www.hbcugala.com/wp-content/uploads/2017/12/Luxury-Background-Photos-Hd-60-For-Hd-Computer-Backgrounds-with-Background-Photos-Hd.jpg" alt="Generic placeholder image">
                 </div>
             </div>
 
